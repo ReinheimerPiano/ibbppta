e36323d3fed9a3446bde7795b5983d457aa9ec65
ReinheimerPiano <pianomatheus@gmail.com>
2020-09-15 10:56:30 -0300

    contact form

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -14,24 +14,24 @@ export default class Index extends React.Component {
     this.state = { isValidated: false };
   }
 
-  // handleChange = (e) => {
-  //   this.setState({ [e.target.name]: e.target.value });
-  // };
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-  // handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   const form = e.target;
-  //   fetch("/", {
-  //     method: "POST",
-  //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-  //     body: encode({
-  //       "form-name": form.getAttribute("name"),
-  //       ...this.state,
-  //     }),
-  //   })
-  //     .then(() => navigate(form.getAttribute("action")))
-  //     .catch((error) => alert(error));
-  // };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...this.state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute("action")))
+      .catch((error) => alert(error));
+  };
 
   render() {
     return (
@@ -43,9 +43,10 @@ export default class Index extends React.Component {
               <form
                 name="contact"
                 method="post"
-                target="_blank"
-                action="https://formsubmit.co/ibbppta@gmail.com"
-                // onSubmit={this.handleSubmit}
+                action="/contact/thanks/"
+                data-netlify="true"
+                data-netlify-honeypot="bot-field"
+                onSubmit={this.handleSubmit}
               >
                 {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
                 <input type="hidden" name="form-name" value="contact" />
